9ccc39146242ac15fb35195b8342f739440023a1
Avery <LegolasdeAda@163.com>
2025-05-08 15:32:29 +0800

    Update rawdata_process.sh

--- rawdata_process.sh
+++ rawdata_process.sh
@@ -1,36 +1,35 @@
 #!/bin/bash
 
-rootpath=/share/home/Z-RNA
-Datapath=/share/home/Z-RNA/rawdata
+rootpath=Z-RNA
+Datapath=Z-RNA/rawdata
+refpath=Z-RNA/ref
 
 cd ${rootpath}
 mkdir fastqc fastp star plasmid rmplasmid
 
 # Quality testing
 cd ${Datapath}
-ls -d *[0-9]|while read id;
+ls -d *|while read id;
 do
 	fastqc -t 8 -o ${rootpath}/fastqc/ ${Datapath}/${id}/${id}_1.fastq.gz
 	fastqc -t 8 -o ${rootpath}/fastqc/ ${Datapath}/${id}/${id}_2.fastq.gz;
 done
 
 # remove adapter
-ls -d *[0-9]|while read id;
+ls -d *|while read id;
 do 
 	fastp -i ${Datapath}/${id}/${id}_1.fastq.gz -o ${rootpath}/fastp/${id}_1.clean.fastq.gz -I ${Datapath}/${id}/${id}_2.fastq.gz -O ${rootpath}/fastp/${id}_2.clean.fastq.gz -q 25 -u 10  -l 50 -w 10 -p;
 done
 
 # STAR
 # generate index
-cd ${rootpath}/plasmid/
-less ~/ref/human/gencode.v42.primary_assembly.annotation.gtf >>plasmid.gtf
-less ~/ref/human/GRCh38.primary_assembly.genome.fa >>plasmid.fa
+cd ${refpath}
 
 STAR --runMode genomeGenerate \
 --runThreadN 10 \
 --genomeDir ${rootpath}/plasmid/plasmid_index \
---genomeFastaFiles ${rootpath}/plasmid/plasmid.fa     \
---sjdbGTFfile ${rootpath}/plasmid/plasmid.gtf \
+--genomeFastaFiles ${refpath}/plasmid.fa     \
+--sjdbGTFfile ${refpath}/plasmid.gtf \
 --sjdbOverhang 100
 
 # mapping
@@ -62,7 +61,7 @@ cd ${rootpath}/star
 mkdir removeP
 #bam
 ls *Aligned.sortedByCoord.out.bam|while read id;do samtools index -b -@ 10 ${id};done
-ls *Aligned.sortedByCoord.out.bam|while read id;do samtools idxstats ${id} | cut -f1 | grep -v "^chrP$" >> all_chromosomes.txt;done
+ls *Aligned.sortedByCoord.out.bam|while read id;do samtools idxstats ${id} | cut -f1 | grep -v "^chrPlasmid$" >> all_chromosomes.txt;done
 less all_chromosomes.txt |sort -u > keep_chromosomes.txt
 ls *Aligned.sortedByCoord.out.bam|while read id;do samtools view -@ 10 -b ${id} $(cat keep_chromosomes.txt) > removeP/${id%_*}-P_Aligned.sortedByCoord.out.bam;done
 #fastq
